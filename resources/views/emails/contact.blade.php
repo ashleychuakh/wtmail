807a47e0dd15f613e191ab1194cb529d99719d7c
Zane J. Chua <zane@webtailors.sg>
2016-06-21 19:21:00 +0800

    Update Email Template

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title>WebTailors Client Enquiry</title>
+        <title>Lead Enquiry</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
     <body style="margin: 0; padding: 0;">
@@ -15,7 +15,7 @@
                                 <table border="0" cellpadding="0" cellspacing="0" width="100%">
                                     <tr>
                                         <td style="color: #153643; font-family: Arial, sans-serif; font-size: 24px; padding-bottom: 10px;">
-                                            <b>Radiance Lead Enquiry</b>
+                                            <b>Lead Enquiry</b>
                                         </td>
                                     </tr>
                                     <tr>
